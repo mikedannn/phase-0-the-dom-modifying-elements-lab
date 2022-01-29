302855f2ea3c45e4cd67cb33856e2a8a45b75171
Michael <michael.daniels1013@gmail.com>
2022-01-29 16:37:57 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+var myobj = document.getElementById("main");
+myobj.remove()
+
+
+let newHeader = document.createElement("H1");
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+newHeader.body.append("H1");
+
+
+// element.append(newHeader);
+
+
+// let h1Node = document.createElement('h1');
+// h1Node.id = 'myId';
+// h1Node.textContent = 'YOUR-NAME is the champion';
+// document.body.appendChild(h1Node);
